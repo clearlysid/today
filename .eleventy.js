b8f0296afde7989260778b15473bf465293b3a6f
Siddharth Jha <yourstrulysiddharth@gmail.com>
2021-06-30 00:16:38 +0530

    💣 removed fluff and unnecessary files

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,7 @@
 const NotionRenderer = require("./_includes/notion");
-const CloudinaryRenderer = require("./_includes/cloudinary");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("style.css");
-	eleventyConfig.addPassthroughCopy("favicons/");
-
-	// cloudinary image shortcode
-	eleventyConfig.addShortcode("cloudimage", (url, alt) =>
-		CloudinaryRenderer(url, alt)
-	);
-
-	// Notion Renderer shortcode
 	eleventyConfig.addShortcode("notion", (blocks) => NotionRenderer(blocks));
 
 	return {
